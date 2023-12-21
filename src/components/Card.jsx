c6662a24450eac8608de4025762c22dba6eafd25
DineshGowthamK <dineshgowtham33@gmail.com>
2023-12-21 10:45:13 +0530

    Completed

---
+++ src/components/Card.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+
+function Card({ cart, setCart, data }) {
+  const [isAddButton, setIsAddButton] = useState(true);
+  return (
+    <div className="col mb-5">
+      <div className="card h-100">
+        <div
+          className="badge bg-dark text-white position-absolute"
+          style={{ top: "0.5rem", right: "0.5rem" }}
+        >
+          {/* Checking whether the product has sale button or not and the text sale is displayed */}
+          {data.isSale ? data.sale : null}
+        </div>
+        <img
+          className="card-img-top"
+          src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+          alt="..."
+        />
+        <div className="card-body p-4">
+          <div className="text-center">
+            <h5 className="fw-bolder">{data.productName}</h5>
+            <div className="d-flex justify-content-center small text-warning mb-2">
+              <div
+                className={
+                  data.productName === "Special Item" ||
+                  data.productName === "Popular Item"
+                    ? "bi-star-fill"
+                    : ""
+                }
+              ></div>
+              <div
+                className={
+                  data.productName === "Special Item" ||
+                  data.productName === "Popular Item"
+                    ? "bi-star-fill"
+                    : ""
+                }
+              ></div>
+              <div
+                className={
+                  data.productName === "Special Item" ||
+                  data.productName === "Popular Item"
+                    ? "bi-star-fill"
+                    : ""
+                }
+              ></div>
+              <div
+                className={
+                  data.productName === "Special Item" ||
+                  data.productName === "Popular Item"
+                    ? "bi-star-fill"
+                    : ""
+                }
+              ></div>
+              <div
+                className={
+                  data.productName === "Special Item" ||
+                  data.productName === "Popular Item"
+                    ? "bi-star-fill"
+                    : ""
+                }
+              ></div>
+            </div>
+            <span
+              className={
+                data.isPrice1 ? "text-muted text-decoration-line-through" : ""
+              }
+            >
+              {data.price1}
+            </span>
+            {data.price2}
+          </div>
+        </div>
+        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+          <div className="text-center">
+            {isAddButton ? (
+              <button
+                className="btn btn-outline-dark mt-auto"
+                onClick={() => {
+                  setCart(cart + 1);
+                  setIsAddButton(false);
+                }}
+              >
+                Add To Cart
+              </button>
+            ) : (
+              <button
+                className="btn btn-outline-dark mt-auto"
+                onClick={() => {
+                  setCart(cart - 1);
+                  setIsAddButton(true);
+                }}
+              >
+                Remove To Cart
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
+
+// {() => {
+//     if (isAddButton) {
+//       setCart(cart + 1);
+//     } else {
+//       setCart(cart - 1);
+//     }
+//     setIsAddButton(!isAddButton);
+//   }}
+//{isAddButton ? "Add to Cart" : "Remove from Cart"}
